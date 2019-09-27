139a3a0ac0f33341600b4911ea375200eaf52c2c
Weston Harder <mrwestonharder@gmail.com>
2019-09-26 19:59:25 -0500

    Added print statements

--- Safecross_VibrationMotors.py
+++ Safecross_VibrationMotors.py
@@ -1,12 +1,18 @@
 import RPi.GPIO as GPIO
 import time
 
+print("Setting pin numbering")
 GPIO.setmode(GPIO.BOARD)
 
+print("Assigning pins")
 GPIO.setup(15, GPIO.OUT)
+
+print("pin 15 high")
 GPIO.output(15,GPIO.HIGH)
 
+print("sleeping...")
 time.sleep(5)
 
+print("pin 15 low")
 GPIO.output(15,GPIO.LOW)
 GPIO.cleanup()
