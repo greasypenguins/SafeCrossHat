198396c071f49e2abead570f57b9b42f77402f99
btlor <blor94@gmail.com>
2019-10-03 19:56:38 -0500

    add time.time

--- Safecross_VibrationMotors.py
+++ Safecross_VibrationMotors.py
@@ -21,11 +21,16 @@ GPIO.cleanup()
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(15, GPIO.OUT)
+seconds = 154925769.55490535
+local_time = time.ctime(seconds)
+print(local_time)
 
-for x in range(20):
-    if x > 5 and x < 15:
+for seconds in range(20):
+    if x > 5 and x < 14:
         GPIO.output(15, GPIO.HIGH)
+        print("pin 15 ON")
     else:
         GPIO.output(15,GPIO.LOW)
+        print("pin 15 low")
 GPIO.cleanup()
         
